da3fa9f5a0cfd3cff412a18734ecd32dbba001ac
Andrew Bialecki <andrew.bialecki@gmail.com>
2011-12-20 15:56:15 -0500

    first commit

---
+++ server.py
@@ -0,0 +1,31 @@
+import json
+
+from bottle import route, request, run, static_file
+
+MAPPING = {
+    '/video/basic-addition' : 'AuX7nPBqDts.ogv',
+    '/video/level-2-addition' : 't2L3JFOqTEk.ogv',
+    # '/video/addition-3' : '27Kp7HJYj2c.ogv',
+    '/video/addition-3' : 'e_SpXIw_Qts.ogv',
+    '/video/addition-4' : 'fOXo4p4WDKM.ogv',
+    # '/video/basic-subtraction' : 'aNqG4ChKShI.ogv',
+    '/video/basic-subtraction' : 'incKJchBCLo.ogv',
+    '/video/subtraction-2' : 'ZaqOUE3H1mE.ogv',
+    '/video/subtraction-3---introduction-to-borrowing-or-regrouping' : 'GBtcGO44e-A.ogv',
+    '/video/alternate-mental-subtraction-method' : 'omUfrXtHtN0.ogv',
+    '/video/level-4-subtraction' : 'fWan_T0enj4.ogv',
+    '/video/why-borrowing-works' : 'SxZUFA2SGX8.ogv',
+    '/video/adding-decimals--old' : '0mOH-qNGM7M.ogv',
+    '/video/subtracting-decimals--old' : 'mvOkMYCygps.ogv',
+}
+
+@route('/find/video')
+def find_video():
+    return {
+        'success' : True,
+        'data' : {
+            'url' : '/static/video/%s' % MAPPING[request.query.path],
+        }
+    }
+
+run(host='localhost', port=8081)
