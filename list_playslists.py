f799d8587b868fadbf70cc0a7bd21523175ee9f8
Andrew Bialecki <andrew.bialecki@gmail.com>
2011-12-22 14:56:19 -0500

    Fixing issues.

--- list_playslists.py
+++ list_playslists.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python
-
 import json
 
 import requests
 
 if __name__ == '__main__':
 
-    playlist = options.playlist
-    format = 'ogv'
-
     response = requests.get('http://www.khanacademy.org/api/playlists')
     playlists = json.loads(response.content)
     
     for playlist in playlists:
-        print '%s with %s video(s)' % (playlist, playlist)
-
-http://www.khanacademy.org/api/playlists
+        print playlist['title']
